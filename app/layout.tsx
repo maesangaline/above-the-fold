8901e6228018a1a4cfb20b704f5fa7faa5710e10
Mae Sangaline <107646143+maesangaline@users.noreply.github.com>
2025-06-09 20:21:05 -0400

    Enable dark mode for the application

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { ThemeProvider } from '@/components/theme-provider'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Above the Fold',
+  description: 'A tool to make header creation a breeze',
   generator: 'v0.dev',
 }
 
@@ -13,8 +14,17 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
